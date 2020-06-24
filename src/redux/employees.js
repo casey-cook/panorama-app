446a684464e46def6f884aee9d9e256fb87e935f
Casey Cook <cook.caseya@gmail.com>
2020-06-24 15:16:14 -0400

    code cleanup

--- src/redux/employees.js
+++ src/redux/employees.js
@@ -2,54 +2,47 @@ import { EMPLOYEES } from '../shared/employeeData';
 import * as ActionTypes from './ActionTypes';
 
 export const Employees = (state = EMPLOYEES, action) => {
-  switch (action.type) {
+	switch (action.type) {
+		//ADDING EMPLOYEE
+		case ActionTypes.EMPLOYEE_ADDED:
+			const newId = state.length;
+			const newName = action.payload.name;
 
-    //ADDING EMPLOYEE  
-    case ActionTypes.EMPLOYEE_ADDED:
+			//Building new employee
+			const newEmployee = {
+				id: newId,
+				name: newName,
+				photo: 'assets/img/newEmployee.png',
+				totalScore: function () {
+					return (
+						(this.performanceAreaScores[0] +
+							this.performanceAreaScores[1] +
+							this.performanceAreaScores[2]) /
+						3
+					).toFixed(1);
+				},
+				performanceAreas: {
+					0: 'Customer Service',
+					1: 'Attendance',
+					2: 'Cash Handling',
+				},
+				performanceAreaScores: {
+					0: '',
+					1: '',
+					2: '',
+				},
+				reviews: [],
+			};
 
-      const newId = (state.length);
-      const newName = action.payload.name;
+			return [...state, newEmployee];
 
-      //Building new employee
-      const newEmployee = {
-        id: newId,
-        name: newName,
-        photo: 'assets/img/newEmployee.png',
-        totalScore: function () {
-          return (
-            (this.performanceAreaScores[0] +
-              this.performanceAreaScores[1] +
-              this.performanceAreaScores[2]) /
-            3
-          ).toFixed(1);
-        },
-        performanceAreas: {
-          0: 'Customer Service',
-          1: 'Attendance',
-          2: 'Cash Handling',
-        },
-        performanceAreaScores: {
-          0: '',
-          1: '',
-          2: '',
-        },
-        reviews: [
-  
-        ],
-      };
+		//REMOVING EMPLOYEE
+		case ActionTypes.EMPLOYEE_REMOVED:
+			return [
+				...state.filter((employee) => employee.name !== action.payload.name),
+			];
 
-      return [
-        ...state, newEmployee
-      ]
-
-    //REMOVING EMPLOYEE
-    case ActionTypes.EMPLOYEE_REMOVED:
-
-      return [...state.filter(employee => employee.name !== action.payload.name)];
-
-    default:
-      return state;
-  }
-}
-
-
+		default:
+			return state;
+	}
+};
--- src/redux/reviews.js
+++ src/redux/reviews.js
@@ -2,115 +2,110 @@ import { EMPLOYEES } from '../shared/employeeData';
 import * as ActionTypes from './ActionTypes';
 
 export const Reviews = (state = EMPLOYEES, action) => {
-  switch (action.type) {
+	switch (action.type) {
+		//DELETING REVIEW
+		case ActionTypes.REVIEW_REMOVED:
+			const empCopy = state.find(
+				(employee) => employee.name === action.payload.review.name
+			);
 
-    //DELETING REVIEW
-    case ActionTypes.REVIEW_REMOVED:
-    
-      const empCopy = state.find(employee => employee.name === action.payload.review.name)
+			const newReviews = empCopy.reviews.filter(
+				(review) => review.id !== action.payload.review.id
+			);
 
-      const newReviews = empCopy.reviews.filter(review => review.id !== action.payload.review.id)
+			empCopy.reviews = newReviews;
 
-      empCopy.reviews = newReviews;
+			state[empCopy.id] = empCopy;
 
-      state[empCopy.id] = empCopy;
+			return [...state];
 
-      return [
-        ...state
-      ]
+		//CREATING REVIEW
+		case ActionTypes.REVIEW_CREATED:
+			//Make copy of employee
+			const employeeCopy = state.find(
+				(employee) => employee.name === action.payload.employeeName
+			);
 
-    //CREATING REVIEW
-    case ActionTypes.REVIEW_CREATED:
+			//Extract what we need pre-newReview-build
+			const newReviewId = employeeCopy.reviews.length;
 
-      //Make copy of employee
-      const employeeCopy = state.find(employee => employee.name === action.payload.employeeName)
+			const newPageNumber = (function () {
+				let counter = 0;
+				state.map((employee) => {
+					counter += employee.reviews.length;
+					return counter;
+				});
+				return counter;
+			})();
 
-      //Extract what we need pre-newReview-build
-      const newReviewId = employeeCopy.reviews.length;
+			const dateSplit = action.payload.date.split('-');
+			const revYear = dateSplit[0];
+			const revMonth = dateSplit[1];
+			const revDay = dateSplit[2];
 
-      const newPageNumber = (function() {
-        let counter = 0;
-        state.map(employee=>{
-          counter += employee.reviews.length
-          return counter;
-        })
-        return counter;
-      })();
-      
-      const dateSplit = action.payload.date.split('-')
-      const revYear = dateSplit[0]
-      const revMonth = dateSplit[1]
-      const revDay = dateSplit[2]
+			//Build new review
+			const newReview = {
+				employeeId: employeeCopy.id,
+				id: newReviewId,
+				reviewPage: newPageNumber,
+				name: action.payload.employeeName,
+				date: {
+					year: revYear,
+					month: revMonth,
+					day: revDay,
+				},
+				areas: {
+					0: 'Customer Service',
+					1: 'Attendance',
+					2: 'Cash Handling',
+				},
+				scores: {
+					0: '',
+					1: '',
+					2: '',
+				},
+				notes: `This is a placeholder for a newly schedled review.`,
+				complete: false,
+			};
 
-      //Build new review
-      const newReview = {
-        employeeId: employeeCopy.id,
-          id: newReviewId,
-          reviewPage: newPageNumber,
-          name: action.payload.employeeName,
-          date: {
-            year: revYear,
-            month: revMonth,
-            day: revDay,
-          },
-          areas: {
-            0: 'Customer Service',
-            1: 'Attendance',
-            2: 'Cash Handling',
-          },
-          scores: {
-            0: '',
-            1: '',
-            2: '',
-          },
-          notes: `This is a placeholder for a newly schedled review.`,
-          complete: false,
-      }
+			//Push review to employee copy
+			employeeCopy.reviews = [...employeeCopy.reviews, newReview];
 
-      //Push review to employee copy
-      employeeCopy.reviews = [...employeeCopy.reviews, newReview]
+			//Re-assign copy of employee (with new review info) into state version of same employee
+			state[employeeCopy.id] = employeeCopy;
 
-      //Re-assign copy of employee (with new review info) into state version of same employee
-      state[employeeCopy.id] = employeeCopy;
+			return [...state];
 
-      return [
-        ...state
-      ]
+		//COMPLETING REVIEW
+		case ActionTypes.REVIEW_COMPLETED:
+			//copying payload, maybe un-needed, but does make code slightly more succint overall
+			const review = action.payload;
 
-    
-    //COMPLETING REVIEW  
-    case ActionTypes.REVIEW_COMPLETED:
+			//Make copy of employee whose review has been completed
+			const employee = state[review.employeeId];
 
-      //copying payload, maybe un-needed, but does make code slightly more succint overall
-      const review = action.payload;
-      
-      //Make copy of employee whose review has been completed
-      const employee = state[review.employeeId]
-      
-      //Convert payload into same structure as receiving object 
-      const forMerge = {
-        id: review.reviewId,
-        name: review.name,
-        scores: {
-          0: parseInt(review.area1),
-          1: parseInt(review.area2),
-          2: parseInt(review.area3)
-        },
-        notes: review.notes,
-        complete: true
-      }
+			//Convert payload into same structure as receiving object
+			const forMerge = {
+				id: review.reviewId,
+				name: review.name,
+				scores: {
+					0: parseInt(review.area1),
+					1: parseInt(review.area2),
+					2: parseInt(review.area3),
+				},
+				notes: review.notes,
+				complete: true,
+			};
 
-      //merge old review with review info from payload
-      Object.assign(employee.reviews[review.reviewId], forMerge);
+			//merge old review with review info from payload
+			Object.assign(employee.reviews[review.reviewId], forMerge);
 
-      //re-assign copy of employee (with new review info) into state version of same employee
-      state[review.employeeId] = employee;
-      
-      return [
-        ...state, 
-      ]
+			//re-assign copy of employee (with new review info) into state version of same employee
+			state[review.employeeId] = employee;
 
-      default:
-      return state;
-  }
-}
+			return [...state];
+
+		default:
+			return state;
+	}
+};
