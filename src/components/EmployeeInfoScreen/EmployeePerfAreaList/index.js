8b09c40da2ec38d711c58b2485b3d345338c91a8
Casey Cook <cook.caseya@gmail.com>
2020-06-25 17:27:00 -0400

    fixed pointer bug on empinfo perf areas

--- src/components/EmployeeInfoScreen/EmployeePerfAreaList/index.js
+++ src/components/EmployeeInfoScreen/EmployeePerfAreaList/index.js
@@ -6,7 +6,7 @@ const empRowStylePerformance = {
 	borderRadius: 6,
 	boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
 	color: '#126872',
-	cursor: 'pointer',
+	// cursor: 'default',
 };
 
 const scoreStyle = {
